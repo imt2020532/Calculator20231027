cc03e7547edee6178215f67f84933ff59d6868b8
imt2020532 <Anwit.Damale@iiitb.ac.in>
2023-10-28 03:53:59 +0530

    Testing Webhook...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ public class Main {
         Scanner myObj = new Scanner(System.in);
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         while(true) {
+            System.out.println("Testing for the webhook... ");
             System.out.println("--Welcome to Calculator--");
             System.out.println("Choose your operation");
 
